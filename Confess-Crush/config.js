596a45a5dd70600c6aad82f4d9b707209b034b9c
loinguyenqzz <73170897+loinguyenqzz@users.noreply.github.com>
2021-11-29 01:57:04 +0700

    Add files via upload

---
+++ Confess-Crush/config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "yêu Hương nhiều nhiều",
+    introTitle: 'Hương à !!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Tiếp tục nào',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Xung quanh em còn rất nhiều người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Tất nhiên là phải yêu anh rồi hí hí',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 8h, anh qua đón đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/loinguyenqzz/' 
+}
